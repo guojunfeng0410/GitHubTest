c137fdd3ad95c30778221358cad97a17e17203c6
yaojin <guojunfeng0401@qq.com>
2017-09-20 23:08:15 +0800

    Creat SecondActivity

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //code B
     //
     //  Code A
     //
